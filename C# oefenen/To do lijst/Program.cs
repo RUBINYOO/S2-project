e82eb23b5d27520ee1e6684fc76f2b6fb4238e69
Ruben <rikkenruben@gmail.com>
2026-03-31 10:11:19 +0200

    .
    .

---
+++ C# oefenen/To do lijst/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace To_do_lijst
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<string> toDoList = new List<string>();
+			bool doorgaan = true;
+
+			while (doorgaan)
+			{
+				Console.WriteLine("=== To-Do lijst ===");
+				Console.WriteLine("1. Taak toevoegen");
+				Console.WriteLine("2. Taken bekijken");
+				Console.WriteLine("3. Taak verwijderen");
+				Console.WriteLine("4. Stoppen");
+
+				Console.Write("Maak een keuze: ");
+				string keuze = Console.ReadLine();
+				Console.Clear();
+
+				switch (keuze)
+				{
+					case "1":
+						Console.WriteLine("Voeg een taak toe en druk op ENTER!");
+
+						toDoList.Add(Console.ReadLine());
+						Console.Clear();
+
+						Console.WriteLine("Activiteit toegevoegd!");
+						Thread.Sleep(3000);
+						Console.Clear();
+						break;
+
+					case "2":
+						if (toDoList.Count == 0)
+						{
+							Console.WriteLine("Er zijn nog geen taken toegevoegd!");
+							Console.ReadKey();
+							Console.Clear();
+						}
+						else
+						{
+							Console.WriteLine("=== Jouw taken ===");
+							for (int i = 0; i < toDoList.Count; i++)
+							{
+								Console.WriteLine($"{i + 1}. {toDoList[i]}");
+							}
+							Console.ReadKey();
+							Console.Clear();
+						}
+						break;
+
+					case "3":
+						if (toDoList.Count == 0)
+						{
+							Console.WriteLine("Er zijn nog geen taken toegevoegd!");
+							Console.ReadKey();
+							Console.Clear();
+							break;
+						}
+
+						Console.WriteLine("Welke taak wil je verwijderen?");
+						for (int i = 0; i < toDoList.Count; i++)
+						{
+							Console.WriteLine($"{i + 1}. {toDoList[i]}");
+						}
+
+						Console.WriteLine();
+						Console.Write("Nummer: ");
+						if (int.TryParse(Console.ReadLine(), out int index) && index >= 1 && index <= toDoList.Count)
+						{
+							toDoList.RemoveAt(index - 1);
+							Console.Clear();
+							Console.WriteLine("Taak verwijderd!");
+							Console.ReadKey();
+							Console.Clear();
+						}
+						else
+						{
+							Console.WriteLine("Ongeldige keuze!");
+						}
+
+						break;
+					case "4":
+						doorgaan = false;
+						break;
+				}
+			}
+			Console.WriteLine("Houdoe en bedankt!");
+		}
+	}
+}
---
+++ C# oefenen/kaasje/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kaasje
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            //if
+            if (age < 18)
+            {
+                Console.WriteLine("Your ticket price is $5");
+            }
+            //else if
+            else if (age > 65 && age <2000)
+            {
+                Console.WriteLine("You are a senior, your ticket is $20");
+            }
+
+            else if (age > 2000)
+            {
+				Console.WriteLine("You are to old!");
+			}
+
+            //else
+            else
+            {
+                Console.WriteLine("Your a adult, your ticket is $10");
+            }
+        }
+    }
+}
